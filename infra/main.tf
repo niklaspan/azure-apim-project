2e3135a3c9ab82b9dc0561c1bb6c8bfa28f12074
Niklas Panov <nicke_panov@hotmail.com>
2026-04-19 15:39:28 +0200

    feat: add APIM resource to main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -16,3 +16,12 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_api_management" "main" {
+  name                = var.apim_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  publisher_name      = "Niklas Panov"
+  publisher_email     = "niklaspanov@gmail.com"
+  sku_name            = "Developer_1"
+}
